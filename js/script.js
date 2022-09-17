94c3c4c0239c0bff4e6cccd571368f4adfcafea9
Kanstantsin <kst.code@gmail.com>
2022-09-17 21:08:31 +0300

    feat: add send form

--- js/script.js
+++ js/script.js
@@ -246,19 +246,30 @@ window.addEventListener('DOMContentLoaded', () => {
             let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
             statusMessage.textContent = message.loading;
-            form.appendChild(statusMessage);
+            form.append(statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'multipart/form-data');
+            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            request.send(formData);
+            const obj = {};
+            formData.forEach((value, key) => {
+                obj[key] = value;
+            });
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
             
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
                 } else {
                     statusMessage.textContent = message.failure;
                 }
